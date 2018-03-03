50e8c84ba40ac08a11d7b16796bb505a88dd6c0e
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-03 10:57:04 +0000

    Adds metadata

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Metalsmith = require("metalsmith");
 
 Metalsmith(__dirname)
+    .metadata({
+        sitename: "Project Longtail"
+    })
     .build(error => {
         if (error) {
             throw error;
